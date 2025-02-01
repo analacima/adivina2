53202009bb3534cd0baf49efc7b3988d2d7e9143
Ana <ana.lacima@gmail.com>
2025-02-01 13:46:01 +0100

    instalado vite, gh-pages

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+//import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <header>
+        <h1>Guess My Number!</h1>
+        <p className="between">(Between 1 and 20)</p>
+        <button className="btn again">Again!</button>
+        <div className="number">?</div>
+      </header>
+      <main>
+        <section className="left">
+          <input type="number" className="guess" />
+          <button className="btn check">
+            Check!
+          </button>
+        </section>
+        <section className="right">
+          <p className="message">Start guessing...</p>
+          <p className="label-score">
+            💯 Score: <span className="score">20</span>
+          </p>
+          <p className="label-highscore">
+            🥇 Highscore: <span className="highscore">0</span>
+          </p>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
