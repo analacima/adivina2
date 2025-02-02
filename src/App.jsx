928c06c714de1d70b1230634c81d9e2882741db5
Ana <ana.lacima@gmail.com>
2025-02-02 20:33:20 +0100

    guarda highscore, deshabilita check, bug random

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,56 @@
 import { useState, useRef } from "react";
 import "./App.css";
 
-
-const number = Math.trunc(Math.random() * 20) + 1;
 const msg_start = "Adivina el número...";
 const printGuess = "?";
 const maxScore = 20;
+let myNumber = Math.trunc(Math.random() * maxScore) + 1;
 
 function App() {
+  let highs = localStorage.getItem("highscore") || 0;
   const [score, setScore] = useState(20);
-  const [highscore, setHighscore] = useState(0);
+  const [highscore, setHighscore] = useState(highs);
   const inputRef = useRef(null);
   const [message, setMessage] = useState(msg_start);
   const [guess, setGuess] = useState(printGuess);
+
   
   const handleAgain = () => {
     setScore(20);
     setMessage(msg_start);
     setGuess(printGuess);
+    myNumber = Math.trunc(Math.random() * maxScore) + 1;
     inputRef.current.value = "";
+    inputRef.current.disabled = false;
     inputRef.current.focus();
   };
+
+ 
 
   const handleCheck = () => {
     console.log(inputRef.current.value);
     const inputNumber = Number(inputRef.current.value);
-    console.log(number);
+    console.log(myNumber);
     if (inputNumber < 1 || inputNumber > maxScore) {
       setMessage("El número debe estar entre 1 y " + maxScore);
-    } else if (inputNumber!==number) {
+    } else if (inputNumber!==myNumber) {
       if (score > 1) {
-        if (inputNumber > number) {
+        if (inputNumber > myNumber) {
           setMessage("📈 ¡Demasiado alto!");
         } else {
           setMessage("📉 ¡Demasiado bajo!");
         }
       } else {
         setMessage("💥 ¡Has perdido!");
+         inputRef.current.disabled = true;
       }
       setScore(score - 1);
     } else {
       setMessage("🎉 ¡Lo has adivinado!");
-      setGuess(number);
+      setGuess(myNumber);
       if (score > highscore) {
         setHighscore(score);
+        localStorage.setItem("highscore", score);
       }
     }
   };
@@ -59,7 +66,7 @@ function App() {
       <main>
         <section className="left">
           <input type="number" className="guess" ref={inputRef}/>
-          <button className="btn check" onClick={handleCheck}>Check!</button>
+          <button className="btn check" onClick={handleCheck} >Check!</button>
         </section>
         <section className="right">
           <p className="message">{message}</p>
